ab1c9388c90d1dc9d30c97899f8e1533da8f1a2b
Dango <jason543211@hotmail.com>
2022-11-07 16:02:26 -0800

    feat: notification

---
+++ src/common/notification.ts
@@ -0,0 +1,44 @@
+import { toast } from 'react-toastify'
+
+export const handleError = (error: Error | any) => {
+  const message = error?.data?.message || error?.message
+  toast(message, {
+    type: 'error',
+    position: 'bottom-left',
+    autoClose: 2500,
+    icon: '🚨',
+    theme: 'dark',
+  })
+  console.error(message)
+}
+
+export const notifyErrorMessage = (message: string) => {
+  toast(message, {
+    type: 'error',
+    position: 'bottom-left',
+    autoClose: 2500,
+    icon: '🚨',
+    theme: 'dark',
+  })
+  console.log(message)
+}
+
+export const handleSuccess = (message: string) => {
+  toast(message, {
+    position: 'top-right',
+    autoClose: 5000,
+    icon: '🪐',
+    theme: 'dark',
+    draggable: false,
+    closeOnClick: false,
+  })
+}
+
+export const handleInfo = (message: string) => {
+  toast(message, {
+    position: 'top-right',
+    autoClose: 5000,
+    icon: '🔔',
+    theme: 'dark',
+  })
+}
--- src/common/rss3.ts
+++ src/common/rss3.ts
@@ -1,1 +1,13 @@
 import axios from 'axios'
+import { RSS3_API_URL } from './endpoints'
+
+const rss3API = axios.create({
+  baseURL: RSS3_API_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+})
+
+const rss3 = {}
+
+export default rss3
