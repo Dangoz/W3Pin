58ea99559b540d4c16be6d02fa5c22b9847ebfa4
Dango <jason543211@hotmail.com>
2022-11-07 20:48:30 -0800

    chore: debounce

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,20 @@ import '../styles/globals.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import 'react-toastify/dist/ReactToastify.css'
 import type { AppProps } from 'next/app'
+import { ToastContainer } from 'react-toastify'
+import { RainbowKitProvider, darkTheme } from '@rainbow-me/rainbowkit'
+import { WagmiConfig } from 'wagmi'
+import { wagmiClient, chains } from '@/common/wagmi'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <WagmiConfig client={wagmiClient}>
+        <RainbowKitProvider chains={chains} theme={darkTheme()}>
+          <Component {...pageProps} />
+          <ToastContainer />
+        </RainbowKitProvider>
+      </WagmiConfig>
+    </>
+  )
 }
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next'
 
 const Test: NextPage = () => {
-  return <div></div>
+  return (
+    <>
+      <div className="w-full h-screen flex justify-center items-center font-bold"></div>
+    </>
+  )
 }
 
 export default Test
