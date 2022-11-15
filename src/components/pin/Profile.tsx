d9cc28eb300372acd974b45fdc5589e02d1db9ef
Dango <jason543211@hotmail.com>
2022-11-15 00:40:10 -0800

    chore: gradient bg bottom

--- src/components/pin/Profile.tsx
+++ src/components/pin/Profile.tsx
@@ -13,7 +13,7 @@ const Profile: React.FC = () => {
         <div className="flex justify-center items-center w-full h-full gap-10 mt-5">
           {attributes.map((attribute) => (
             <div key={attribute} className="flex flex-col justify-center items-center gap-1">
-              <div className="flex justify-center items-center gradientBG rounded-md p-[0.0625rem]">
+              <div className="flex justify-center items-center gradientBGB rounded-md p-[0.0625rem]">
                 <div className="bg-[#161A42] rounded-md py-2 px-5 font-tomorrow">
                   {cardStore[attribute.toLowerCase() as keyof Card]}
                 </div>
