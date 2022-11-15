fbf53b160f65c6fc3d8e6e897b67a221313f32f7
Dango <jason543211@hotmail.com>
2022-11-15 04:42:26 -0800

    chore: edit bar image button

--- src/components/option/EditBar.tsx
+++ src/components/option/EditBar.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import * as Toolbar from '@radix-ui/react-toolbar'
-import { Cross1Icon, Pencil1Icon } from '@radix-ui/react-icons'
+import { Cross1Icon, Pencil1Icon, ImageIcon } from '@radix-ui/react-icons'
 import Button from '../ui/Button'
 import ToolTip from '../ui/ToolTip'
 
@@ -13,14 +13,23 @@ const EditBar: React.FC<EditBarProps> = ({ toggleEditMode }) => {
     <>
       <Toolbar.Root className="w-5/6 h-11 bg-[rgba(26,26,26,.8)] backdrop-blur-sm rounded-md flex justify-between items-center py-2 px-3 border-bgGrey sticky bottom-2.5">
         {/* left */}
-        <div className="flex justify-center">
+        <div className="flex justify-center gap-3">
           <Toolbar.Button
             className="flex items-center justify-center bg-red-500 hover:bg-red-500/80 rounded-sm w-6 h-6 m-auto"
             onClick={toggleEditMode}
           >
             <Cross1Icon />
           </Toolbar.Button>
-          <Toolbar.Separator className="w-0.5 bg-white/50 mx-3" />
+          <Toolbar.Separator className="w-0.5 bg-white/50 mx-2" />
+
+          <ToolTip message="Description" side="top" delayDuration={750}>
+            <Toolbar.Button
+              className="flex items-center justify-center bg-black border-[1px] border-bgGrey rounded-md w-7 h-7 hover:bg-bgGrey"
+              onClick={toggleEditMode}
+            >
+              <ImageIcon className="w-3.5 h-3.5 text-white" />
+            </Toolbar.Button>
+          </ToolTip>
 
           <ToolTip message="Description" side="top" delayDuration={750}>
             <Toolbar.Button
