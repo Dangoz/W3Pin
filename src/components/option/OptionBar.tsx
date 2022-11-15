103582a1164a7af9ee57f4e00e4f4e7590238ce1
Dango <jason543211@hotmail.com>
2022-11-15 02:21:04 -0800

    chore: adjust option bar

--- src/components/option/OptionBar.tsx
+++ src/components/option/OptionBar.tsx
@@ -8,27 +8,24 @@ interface OptionBarProps {}
 const OptionBar: React.FC<OptionBarProps> = () => {
   return (
     <>
-      <Toolbar.Root className="w-96 h-14 bg-[rgba(26,26,26,.8)] backdrop-blur-sm rounded-md flex py-2 px-2 justify-evenly">
+      <Toolbar.Root className="w-fit h-14 bg-[rgba(26,26,26,.8)] backdrop-blur-sm rounded-md flex py-2 px-2 justify-evenly border-bgGrey">
         <Toolbar.Button asChild>
-          <Button className="mr-2 w-28" size="sm">
-            <RocketIcon className="w-3 h-3 mr-2" />
+          <Button className="mr-2 w-32">
+            <RocketIcon className="w-4 h-4 mr-2" />
             Gift
           </Button>
         </Toolbar.Button>
-        {/* <Toolbar.Separator className='' /> */}
+
         <Toolbar.Button asChild>
-          <Button
-            size="sm"
-            className="mr-2 w-28 bg-gradient-to-l from-gradientOne to-gradientThree hover:from-gradientOne/80 hover:to-gradientThree/80"
-          >
-            <Pencil2Icon className="w-3 h-3 mr-2" />
+          <Button className="mr-2 w-32 bg-gradient-to-l from-gradientOne to-gradientThree hover:from-gradientOne/80 hover:to-gradientThree/80">
+            <Pencil2Icon className="w-4 h-4 mr-2" />
             Edit
           </Button>
         </Toolbar.Button>
 
         <Toolbar.Button asChild>
-          <Button className="w-28" variant="secondary" size="sm">
-            <DownloadIcon className="w-3 h-3 mr-1" />
+          <Button className="w-32" variant="secondary">
+            <DownloadIcon className="w-4 h-4 mr-1" />
             Download
           </Button>
         </Toolbar.Button>
